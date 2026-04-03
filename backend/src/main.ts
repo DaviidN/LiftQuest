385a1342adabcf08afb27fb258eca3c36a7f8702
DaveN <101190750+DaviidN@users.noreply.github.com>
2026-04-03 22:53:03 +0200

    Troubleshooting 502 error on deploy.  n. 21

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3001;
   await app.listen(port, '0.0.0.0'); 
   
+  console.log(`This is the server: ${port}`)
   console.log(`Server running on port ${port}`);
 }
 bootstrap();
