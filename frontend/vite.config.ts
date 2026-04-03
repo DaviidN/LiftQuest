039dee074beb10e2ca333f4b165460cf155640a2
DaveN <101190750+DaviidN@users.noreply.github.com>
2026-04-04 01:04:37 +0200

    Troubleshooting 502 error on deploy.  n. 25

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173,
+    allowedHosts: ['.railway.app'],
+    host: '0.0.0.0',
+    port: 8080
   },
   preview: {
     host: true,
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
-  }
+  },
 })
